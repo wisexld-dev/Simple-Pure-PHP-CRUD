3bc88a0562c4dfc102ba705197de6122a7fbd5d0
shellGhost <159303907+shellGhxst@users.noreply.github.com>
2024-03-21 17:30:37 -0300

    Ajuste logo navbar

--- Components/navbar.php
+++ Components/navbar.php
@@ -1,22 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Minha Empresa</a>
+        <a class="navbar-brand" href="/">Teste - Telecontrol</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link active" href="../Controller/clienteController.php">Clientes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../Controller/clienteController.php">Clientes</a>
+                    <a class="nav-link active" href="../Controller/produtoController.php">Produtos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../Controller/produtoController.php">Produtos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Controller/ordemservicoController.php">Ordem de Serviço</a>
+                    <a class="nav-link active" href="../Controller/ordemservicoController.php">Ordem de Serviço</a>
                 </li>
             </ul>
         </div>
